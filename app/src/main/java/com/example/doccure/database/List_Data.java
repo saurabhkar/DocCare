c482012acc486c7cf532efe28adce2055eae2965
arati sharma <aratis990@gmail.com>
2019-06-25 20:22:58 +0530

    finally done

--- app/src/main/java/com/example/doccure/database/List_Data.java
+++ app/src/main/java/com/example/doccure/database/List_Data.java
@@ -1,16 +1,16 @@
 package com.example.doccure.database;
 
 public class List_Data {
-    // private String id;
+   // private String id;
     private String time;
-    // private String movie;
+   // private String movie;
 
-    public List_Data(){
+public List_Data(){
 
-    }
+}
 
     public List_Data( String time ) {
-        //   this.id = id;
+     //   this.id = id;
         this.time = time;
     }
 
