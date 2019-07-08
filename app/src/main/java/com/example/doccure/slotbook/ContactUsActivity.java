abd2187a51fa11b0857be37c8f183bbaf9fccf66
arati sharma <aratis990@gmail.com>
2019-07-08 13:23:30 +0530

    Contact us added with some changes

---
+++ app/src/main/java/com/example/doccure/slotbook/ContactUsActivity.java
@@ -0,0 +1,15 @@
+package com.example.doccure.slotbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.doccure.R;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
